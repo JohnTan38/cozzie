d0015a0015a2fdd36125a31df365505145a96939
JohnTan38 <113171463+JohnTan38@users.noreply.github.com>
2024-03-30 17:51:24 +0800

    Update app.py

--- Scripts/app.py
+++ Scripts/app.py
@@ -202,7 +202,8 @@ if st.button("Send email"):
             part.set_payload(attachment.read())
 
         file_name=extract_file_name(latest_csv)
-        fmt_file_name = (replace_date_with_status(file_name))
+        #fmt_file_name = (replace_date_with_status(file_name))
+        fmt_file_name = "2024-03-30_AV.csv"
         encoders.encode_base64(part)
         part.add_header(
             "Content-Disposition",
